9dc40f4018f52135452f171589539118e8a8c5b6
Serguei Cambour <serguei.cambour.partner@decathlon.com>
2019-07-16 16:39:44 +0200

    Remove @EnableScheduling annotation, leave log writing

--- src/main/java/hello/TranslationsScheduleTask.java
+++ src/main/java/hello/TranslationsScheduleTask.java
@@ -3,23 +3,20 @@ package hello;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-//@EnableScheduling
 public class TranslationsScheduleTask {
     private static final Logger log = LoggerFactory.getLogger(TranslationsScheduleTask.class);
 
-    /*@Autowired
+    @Autowired
     private JobLauncher jobLauncher;
 
     @Autowired
@@ -29,8 +26,8 @@ public class TranslationsScheduleTask {
     public void importTranslations() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
         // TODO implement PhraseApp translations import scheduling here
 
-        jobLauncher.run(job, new JobParametersBuilder().toJobParameters());
+        //jobLauncher.run(job, new JobParametersBuilder().toJobParameters());
 
         log.info("Fixed rate task - " + System.currentTimeMillis() / 1000);
-    }*/
+    }
 }
